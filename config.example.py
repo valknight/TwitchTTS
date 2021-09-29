21ba8ecf1fb905c3aa19cae13c8e5c76f2bda2e5
mogoh <mv@mogoh.de>
2021-09-29 20:57:16 +0200

    Refactor lookup and move accents to config

--- config.example.py
+++ config.example.py
@@ -1,3 +1,13 @@
 twitch_token = "DONOTSHOWONSTREAM"
 channel = '#vknilive'
 nickname = 'vknilive'
+accents = [
+    {'language': 'en', 'accent': 'com'},
+    {'language': 'en', 'accent': 'com.au'},
+    {'language': 'en', 'accent': 'co.uk'},
+    {'language': 'en', 'accent': 'co.za'},
+    {'language': 'en', 'accent': 'ca'},
+    {'language': 'fr', 'accent': 'fr'},
+    {'language': 'fr', 'accent': 'ca'},
+    {'language': 'es', 'accent': 'es'},
+]
